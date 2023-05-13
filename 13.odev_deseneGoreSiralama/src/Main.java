48b2aada5ce2621b6a7cd866b8d23cc77848bfda
RUKİYE <84405213+RUKIYE-X@users.noreply.github.com>
2023-05-14 00:21:41 +0300

    deseneGoreSiralama
    .....

---
+++ 13.odev_deseneGoreSiralama/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int desen(int number,int temp){
+        if(number>0){
+            System.out.println(number);
+            number = number-5;
+            return desen(number,temp);
+        }else {
+            desen2(number, temp);
+        }return temp;
+    }
+    static int desen2(int number,int temp){
+        if(number< temp){
+            System.out.println(number);
+            number = number + 5;
+            desen2(number,temp);
+        }
+        return number;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("bir sayı giriniz: ");
+        int number,temp ;
+        number = input.nextInt();
+        temp = number;
+        System.out.println(desen(number,temp));
+        /*
+        if(number == 0 || number <0) {
+            number = number +5;
+            System.out.println(number);
+            desen(number,i);
+        }else if(i>0){
+            number = number + 5;
+            System.out.println(number);
+            desen(number,i-1);
+        }else{*/
+    }
+}
